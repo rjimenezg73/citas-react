699bd714bd923fc24e248fef06686a3a26b40818
Roberto Jimenez <robertojimenezg@gmail.com>
2022-11-15 22:14:51 -0600

    Configurando tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.jsx"], // "./src/**/*.jsx" busca en /src/Todas las carpetas/Todos los archivos con extensión jsx, osea, los componentes.
   theme: {
     extend: {},
   },
